b474ac2d22ba929e2096525d8b0c24156fd475cd
Souvik <emailtosvk@gmail.com>
2018-12-10 01:04:23 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+/** @format */
+
+import {AppRegistry} from 'react-native';
+import Menu from './Menu';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => Menu);
