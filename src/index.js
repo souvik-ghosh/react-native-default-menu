d757fecd000c2efb372573f361062d3e79b18c59
Souvik Ghosh <emailtosvk@gmail.com>
2018-12-10 14:40:45 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,44 +1,38 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import {
   View,
   UIManager,
   findNodeHandle,
   TouchableWithoutFeedback,
   ActionSheetIOS,
-  Platform
-} from "react-native";
-import Icon from "react-native-vector-icons/MaterialIcons";
+  Platform,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export default class Menu extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   showActionSheet = () => {
-    const options = ["Cancel", ...this.props.actions];
+    const { actions, destructiveButtonIndex, onPress } = this.props;
+    const options = ['Cancel', ...actions];
     ActionSheetIOS.showActionSheetWithOptions(
       {
         options,
         cancelButtonIndex: 0,
-        destructiveButtonIndex: this.props.destructiveButtonIndexIos + 1 || -1
+        destructiveButtonIndex: destructiveButtonIndex + 1 || -1,
       },
       buttonIndex => {
-        this.props.onPress(options[buttonIndex], buttonIndex - 1);
+        onPress(options[buttonIndex], buttonIndex - 1);
       }
     );
   };
 
   showPopupMenu = () => {
-    UIManager.showPopupMenu(
-      findNodeHandle(this.iconRef),
-      this.props.actions,
-      this.props.onError || (() => console.log("Popup Error")),
-      this.props.onPress
-    );
+    const { actions, onError, onPress } = this.props;
+    const node = findNodeHandle(this.iconRef);
+    UIManager.showPopupMenu(node, actions, onError, onPress);
   };
 
   onPress = () => {
-    if (Platform.OS === "ios") {
+    if (Platform.OS === 'ios') {
       this.showActionSheet();
       return;
     }
@@ -46,13 +40,14 @@ export default class Menu extends Component {
   };
 
   render() {
+    const { color, icon, iconSize } = this.props;
     return (
       <TouchableWithoutFeedback onPress={this.onPress}>
-        <View style={{ alignItems: 'center', justifyContent: "center" }}>
+        <View style={{ alignItems: 'center', justifyContent: 'center' }}>
           <Icon
-            name={"more-vert" || this.props.icon}
-            size={this.props.iconSize || 24}
-            color={"grey" || this.props.color}
+            name={'more-vert' || icon}
+            size={iconSize || 24}
+            color={'grey' || color}
             ref={node => {
               this.iconRef = node;
             }}
